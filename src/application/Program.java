1431086069013fbefc7ec228c99f11febf285ce1
Felipe Mendes <lfelipecmendes@icloud.com>
2024-06-27 12:47:41 -0300

    feat: change moment property type from Date to Instant

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,15 +3,15 @@ package application;
 import entities.Comment;
 import entities.Post;
 
-import java.util.Date;
+import java.time.Instant;
 
 public class Program {
     public static void main(String[] args) {
-        Post post_one = new Post(new Date(), "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
+        Post post_one = new Post(Instant.now(), "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
         post_one.addComment(new Comment("Have a nice trip!"));
         post_one.addComment(new Comment("Wow that's awesome!"));
 
-        Post post_two = new Post(new Date(), "Good night guys", "See you tomorrow", 5);
+        Post post_two = new Post(Instant.now(), "Good night guys", "See you tomorrow", 5);
         post_two.addComment(new Comment("Good night"));
         post_two.addComment(new Comment("May the Force be with you"));
 
--- src/entities/Post.java
+++ src/entities/Post.java
@@ -1,11 +1,14 @@
 package entities;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.List;
 
 public class Post {
-    private Date moment;
+    private Instant moment;
     private String title;
     private String content;
     private Integer likes;
@@ -15,18 +18,18 @@ public class Post {
     public Post() {
     }
 
-    public Post(Date moment, String title, String content, Integer likes) {
+    public Post(Instant moment, String title, String content, Integer likes) {
         this.moment = moment;
         this.title = title;
         this.content = content;
         this.likes = likes;
     }
 
-    public Date getMoment() {
+    public Instant getMoment() {
         return moment;
     }
 
-    public void setMoment(Date moment) {
+    public void setMoment(Instant moment) {
         this.moment = moment;
     }
 
@@ -64,10 +67,12 @@ public class Post {
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").withZone(ZoneId.systemDefault());
+
         sb.append(title).append("\n");
         sb.append(likes);
         sb.append(" Likes - ");
-        sb.append(moment).append("\n");
+        sb.append(dtf.format(moment)).append("\n");
         sb.append(content).append("\n");
         sb.append("Comments:\n");
         for (Comment c : comments) {
